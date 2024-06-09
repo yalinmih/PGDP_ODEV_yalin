0f7e208540630f915c85848963d75254ea9dc9b2
volkan istek <volkanistek@mail.com>
2024-06-09 20:53:15 +0200

    volkan test

--- Q01.java
+++ Q01.java
@@ -4,6 +4,6 @@ public class Q01 {
     public static void main(String[] args) {
         System.out.println("Welcome to Java");
         System.out.println("Welcome to Computer Science");
-        System.out.println("Programming is fun");
+        System.out.println("Programming is fun"); 
     }
 }
